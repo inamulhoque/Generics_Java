130e30ee3e8625d67bba50890e9685383c3595fa
Inamul <inamulhoque097@gmail.com>
2022-06-13 19:59:41 +0530

    Initial: Find maximum of three integers.

---
+++ src/com/inamul/PracticeProblemOnGenerics.java
@@ -0,0 +1,18 @@
+package com.inamul;
+
+public class PracticeProblemOnGenerics{
+    public static <Integer extends Comparable<Integer>> Integer testMaximum(Integer x, Integer y, Integer z){
+        Integer max = x;
+        if (y.compareTo(max)>0){
+            max = y;
+        }
+        if (z.compareTo(max)>0) {
+            max = z;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum is: "+testMaximum(23,55,69));
+    }
+}
