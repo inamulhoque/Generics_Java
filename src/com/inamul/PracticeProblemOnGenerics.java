eb3bf71970ec1c4fc3d4cba58d0fe1bf7bf80992
Inamul <inamulhoque097@gmail.com>
2022-06-14 23:54:58 +0530

    Added: Using generic constructor.

--- src/com/inamul/PracticeProblemOnGenerics.java
+++ src/com/inamul/PracticeProblemOnGenerics.java
@@ -1,20 +1,28 @@
 package com.inamul;
 
-public class PracticeProblemOnGenerics <E>{
-    public static <E extends Comparable<E>> E testMaximum(E x, E y, E z){
-        E max = x;
-        if (y.compareTo(max)>0){
-            max = y;
-        }
-        if (z.compareTo(max)>0) {
-            max = z;
-        }
-        return max;
+public class PracticeProblemOnGenerics<E extends Comparable> {
+    E x, y, z;
+
+    public PracticeProblemOnGenerics(E x, E y, E z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Maximum is: "+testMaximum(4,48,12));
-        System.out.println("Maximum is: "+testMaximum("Banana", "Apple","Peach"));
-        System.out.println("Maximum is: "+testMaximum(22.34,15.12,10.99));
+    public static <E extends Comparable<E>> E testMaximum(E x, E y, E z) {
+        E max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return (max);
     }
-}
+        public static void main (String[]args){
+            System.out.println(testMaximum(25,45,75));
+            System.out.println(testMaximum(36.5,28.0,38.75));
+            System.out.println(testMaximum("Apple","Mango","Banana"));
+        }
+    }
+
