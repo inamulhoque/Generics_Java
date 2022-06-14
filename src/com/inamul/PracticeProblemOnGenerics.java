cb414d235e97bae7c7c3adb69c423076f5e63b40
Inamul <inamulhoque097@gmail.com>
2022-06-15 00:00:39 +0530

    Added: Took 4 arguments.

--- src/com/inamul/PracticeProblemOnGenerics.java
+++ src/com/inamul/PracticeProblemOnGenerics.java
@@ -1,28 +1,32 @@
 package com.inamul;
 
 public class PracticeProblemOnGenerics<E extends Comparable> {
-    E x, y, z;
+    E w,x, y, z;
 
     public PracticeProblemOnGenerics(E x, E y, E z) {
+        this.w = w;
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public static <E extends Comparable<E>> E testMaximum(E x, E y, E z) {
-        E max = x;
+    public static <E extends Comparable<E>> E testMaximum(E w, E x, E y, E z) {
+        E max = w;
+        if (x.compareTo(max) > 0) {
+            max = x;
+        }
         if (y.compareTo(max) > 0) {
             max = y;
         }
-        if (z.compareTo(max) > 0) {
+        if (z.compareTo(max)>0){
             max = z;
         }
         return (max);
     }
         public static void main (String[]args){
-            System.out.println(testMaximum(25,45,75));
-            System.out.println(testMaximum(36.5,28.0,38.75));
-            System.out.println(testMaximum("Apple","Mango","Banana"));
+            System.out.println(testMaximum(25,45,75,100));
+            System.out.println(testMaximum(36.5,28.0,38.75,5.77));
+            System.out.println(testMaximum("Apple","Mango","Banana","Kiwi"));
         }
     }
 
